c1b217032c919acf4121a822e81a31a92f80b4d5
Huy Le Quang <91176131+quagh03@users.noreply.github.com>
2026-06-20 15:00:51 +0700

    initial commit

---
+++ src/main/java/com/huylq/springjpadatabasedeeplearn/SpringJpaDatabaseDeepLearnApplication.java
@@ -0,0 +1,13 @@
+package com.huylq.springjpadatabasedeeplearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaDatabaseDeepLearnApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringJpaDatabaseDeepLearnApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/huylq/springjpadatabasedeeplearn/SpringJpaDatabaseDeepLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huylq.springjpadatabasedeeplearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaDatabaseDeepLearnApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
